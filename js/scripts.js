7148e92ce292e81f16a27eb99dd6c7b850d1f917
Scott McIntire and Sarah Read Brown <scottmcintire90@gmail.com>
2016-03-28 20:50:18 -0700

    added icon and player variables and used .html for appends

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
-
-
+var counter = 0;
+var icon = 'X';
+var player = 'Player 1';
 
 $(document).ready(function() {
-
   $(".1, .2, .3, .4, .5, .6, .7, .8, .9").click(function(event) {
-      $(this).text("X");
-
+    if($(this).html()=="") {
+      if(counter % 2 === 0) {
+        icon = "O";
+        player = 'Player 2';
+      } else {
+        icon = 'X';
+        player = 'Player 1';
+      }
+      $(this).html(icon);
+      counter++
+    }
 
   });
 
