debc8350aaf8faa5948a796b877a2b1f890ab32b
Scott McIntire and Sarah Read Brown <sreadbrown@gmail.com>
2016-03-30 19:09:41 -0700

    add a index that pushes the mark

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 var counter = 0;
 var icon = 'X';
 var player = 'Player 1';
-
+var board = ["","","","","","","","",""];
 
 
 
 $(document).ready(function() {
   $(".square").click(function(event) {
+    var index = $(this).attr('id');
     if($(this).html()=="") {
       if(counter % 2 === 0) {
         icon = "O";
@@ -16,15 +17,18 @@ $(document).ready(function() {
         player = 'Player 1';
       }
       $(this).html(icon);
+      board[index] = icon;
       counter++
+      //board.push(icon);
+      // debugger;
     }
 
   });
 
   $('#restart').click(function() {
     $('.square').html('');
+    // figure out how to reset board array
     counter = 0;
-
   });
 
 
