b89b4b620c13fd1d42c89b7bdc0fdadea73d786f
Scott McIntire and Sarah Read Brown <scottmcintire90@gmail.com>
2016-03-30 20:25:12 -0700

    turn board values into int and builds a broken for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,48 @@
 var counter = 0;
 var icon = 'X';
 var player = 'Player 1';
-var board = ["","","","","","","","",""];
+var boardX = [];
+var boardO = [];
+var winning = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
 
+for (var i = 0; i < winning.length; i++) {
+  for (var j = 0; j < boardX.length; j++) {
+    if (winning[i] == boardX[j]) {
+      var win = true
+    }
+      var win = false
+  }
+}
 
 $(document).ready(function() {
+
   $(".square").click(function(event) {
-    var index = $(this).attr('id');
+
+    var index = parseInt($(this).attr('id'));
     if($(this).html()=="") {
       if(counter % 2 === 0) {
         icon = "O";
         player = 'Player 2';
+        $(this).html(icon);
+        boardO.push(index);
       } else {
         icon = 'X';
         player = 'Player 1';
+        $(this).html(icon);
+        boardX.push(index);
       }
-      $(this).html(icon);
-      board[index] = icon;
       counter++
-      //board.push(icon);
-      // debugger;
     }
-
+    if (board[0] = board[1] = board[2]) {
+      alert("you won!");
+    }
   });
+
 
   $('#restart').click(function() {
     $('.square').html('');
-    // figure out how to reset board array
+      board = [];
     counter = 0;
   });
 
